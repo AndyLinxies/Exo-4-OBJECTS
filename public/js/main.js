0e373851d938c84368afa912c248aa16d3555d5c
AndyLinxies <linxies@gmail.com>
2021-04-30 11:58:33 +0200

    Exo4 objet

---
+++ public/js/main.js
@@ -0,0 +1,28 @@
+// let vieille_dame ={
+//     age : 89,
+//     nom : {
+//         prenom: "Gertrude",
+//         nomDeFamille: "Declercq",
+//     },
+//     moral : "mal",
+//     objet : "canne",
+//     parler (){
+//         if (this.moral=="mal") {
+//             alert(`Vous me dérangez, je vous frappe avec ma ${this.objet} `)
+//         } else if(this.moral=="bien") {
+//             alert(`Bonjour ${vieil_homme.nom}`)
+//         }
+//     }
+// }
+// let vieil_homme = {
+//     nom: "Robert",
+//     adoucir (){
+//         vieille_dame.moral="bien"
+//     } 
+    
+// }
+// vieil_homme.adoucir()
+// vieille_dame.parler(); //on derange car adoucir de viel homme n'a pas encore été declanché (appelé).
+// //Ensuite quand on décomente viel homme elle est de bonne humeur
+
+
